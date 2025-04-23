c8cc9666ebc6bcf3d5abb07b9ac961754a3408f3
Mostafa Zamani <119905696+mstfazmni@users.noreply.github.com>
2025-04-23 16:24:50 -0600

    search input and header img

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
 import { useState, useEffect } from 'react'
 import './App.css'
+import Search from './components/Search';
 
 const App = () => {
-  return(
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-  </h1>
-  )
+
+  const [searchTerm, setSearchTerm] = useState('');
+
+  return (
+    <main>
+        <div className='pattern' />
+        
+        <div className='wrapper'>
+          <header>
+              <img src='./hero-img.png' alt='Hero Banner' />
+              <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without the Hassle</h1>
+          </header>
+
+          <Search searchTerm={searchTerm} setSearchTerm = {setSearchTerm} />
+
+        </div>
+    </main>
+  );
 }
 
 export default App
