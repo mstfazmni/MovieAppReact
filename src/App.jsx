34ddf66c7a6dc4a41d8a8d3e9216174cdc4f1de3
Mostafa Zamani <119905696+mstfazmni@users.noreply.github.com>
2025-04-24 02:30:15 -0600

    appwrite for database

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import './App.css'
 import Search from './components/Search';
 import Spinner from './components/Spinner';
 import MovieCard from './components/MovieCard';
+import { useDebounce } from 'react-use';
+import { updateSearchCount } from './appwrite';
 
 const API_BASE_URL = 'https://api.themoviedb.org/3';
 
@@ -21,13 +23,19 @@ const App = () => {
   const [errorMessage, setErrorMessage] = useState('');
   const [movieList, setMovieList] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [debouncedSearchTerm, setDebouncedSearchTerm] = useState('');
 
-  const fetchMovies = async () => {
+  useDebounce(() => setDebouncedSearchTerm(searchTerm), 500, [searchTerm]);
+
+  const fetchMovies = async (query = '') => {
     setIsLoading(true);
     setErrorMessage('');
 
     try{
-        const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+        const endpoint =  query 
+        ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}` 
+        : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+
         const response = await fetch(endpoint, API_OPTIONS);
 
         if (!response.ok){
@@ -41,6 +49,10 @@ const App = () => {
         }
         setMovieList(data.results || []);
         
+        if(query && data.results.length > 0) {
+          await updateSearchCount(query, data.results[0]);
+        }
+
     } catch(error){
       console.error(`Error fetching movies: ${error}`);
       setErrorMessage('Error fetching movies. Please try later.');
@@ -50,8 +62,9 @@ const App = () => {
   }
 
   useEffect(()=>{
-    fetchMovies();
-  },[])
+    fetchMovies(debouncedSearchTerm);
+  },[debouncedSearchTerm])
+
   return (
     <main>
         <div className='pattern' />
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,7 +6,7 @@ const MovieCard = ({movie:
     return (
         <div className='movie-card'>
             <img 
-            src={poster_path ? `https://image.tmdb.org/t/p/w500/${poster_path}` : '/no-movie.png'}
+            src={poster_path ? `https://image.tmdb.org/t/p/w500/${poster_path}` : '/no-poster.png'}
             alt={title} 
             />
 
