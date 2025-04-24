2c90b981f20b7e1856613a74059f23e8d2a99d45
Mostafa Zamani <119905696+mstfazmni@users.noreply.github.com>
2025-04-23 21:02:42 -0600

    Compeleting fetching movies and Spinner

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import './App.css'
 import Search from './components/Search';
+import Spinner from './components/Spinner';
 
 const API_BASE_URL = 'https://api.themoviedb.org/3';
 
@@ -15,13 +16,15 @@ const API_OPTIONS = {
 }
 
 const App = () => {
-
-
   const [searchTerm, setSearchTerm] = useState('');
-
   const [errorMessage, setErrorMessage] = useState('');
+  const [movieList, setMovieList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const fetchMovies = async () => {
+    setIsLoading(true);
+    setErrorMessage('');
+
     try{
         const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
         const response = await fetch(endpoint, API_OPTIONS);
@@ -30,11 +33,18 @@ const App = () => {
           throw new Error('Failed to fetch movies')
         }
         const data = await response.json();
-        console.log(data);
+        if (data.Response === 'False') {
+          setErrorMessage(data.Error) || 'Failed to fetch movies'; 
+          setMovieList([]);
+          return;
+        }
+        setMovieList(data.results || []);
         
     } catch(error){
       console.error(`Error fetching movies: ${error}`);
       setErrorMessage('Error fetching movies. Please try later.');
+    } finally {
+      setIsLoading(false);
     }
   }
 
@@ -55,8 +65,20 @@ const App = () => {
           </header>
 
           <section className='all-movies'>
-            <h2>All Movies</h2>
-            {errorMessage && <p className='text-red-500'>{errorMessage}</p>}
+            <h2 className='mt-[40px]'>All Movies</h2>
+
+          {isLoading ? (
+            <Spinner />
+          ) : errorMessage ? ( 
+            <p className='text-red-500'>{errorMessage}</p>
+          ) : (
+            <ul>
+              {movieList.map((movie) => (
+                <p key={movie.id} className='text-white'>{movie.title}</p>
+              ))}
+            </ul>
+          )}
+
           </section>
 
         </div>
