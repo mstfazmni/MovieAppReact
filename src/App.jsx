c63ec7077f5e6b93d8c65cd9287174974331c895
Mostafa Zamani <119905696+mstfazmni@users.noreply.github.com>
2025-04-24 02:46:11 -0600

    trending movies created

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Search from './components/Search';
 import Spinner from './components/Spinner';
 import MovieCard from './components/MovieCard';
 import { useDebounce } from 'react-use';
-import { updateSearchCount } from './appwrite';
+import { getTrendingMovies, updateSearchCount } from './appwrite';
 
 const API_BASE_URL = 'https://api.themoviedb.org/3';
 
@@ -24,6 +24,7 @@ const App = () => {
   const [movieList, setMovieList] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [debouncedSearchTerm, setDebouncedSearchTerm] = useState('');
+  const [trendingMovies, setTrendingMovies] = useState([]);
 
   useDebounce(() => setDebouncedSearchTerm(searchTerm), 500, [searchTerm]);
 
@@ -61,9 +62,24 @@ const App = () => {
     }
   }
 
+  const loadTrendingMovies = async () => {
+    try{
+      const movies = await getTrendingMovies();
+      setTrendingMovies(movies);
+      
+    } catch (error){
+      console.log(`Error fetching trending movies: ${error}`);
+    }
+  }
+  
+
   useEffect(()=>{
     fetchMovies(debouncedSearchTerm);
   },[debouncedSearchTerm])
+
+  useEffect(() => {
+    loadTrendingMovies();
+  }, [])
 
   return (
     <main>
@@ -78,8 +94,22 @@ const App = () => {
 
           </header>
 
+          {trendingMovies.length > 0 && (
+            <section className='trending'>
+              <h2>Trending Movies</h2>
+              <ul>
+                  {trendingMovies.map((movie, index) => (
+                    <li key={movie.$id}>
+                        <p>{index + 1}</p>
+                        <img src={movie.poster_url} alt={movie.title} />
+                    </li>
+                  ))}
+              </ul>
+            </section>
+          )}
+
           <section className='all-movies'>
-            <h2 className='mt-[40px]'>All Movies</h2>
+            <h2>All Movies</h2>
 
           {isLoading ? (
             <Spinner />
